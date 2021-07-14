19675ee2caab7a082c065b575bae766ad385b95e
Pratik Talele <pratiktalele22@gmail.com>
2021-07-14 13:00:03 +0530

    EmployeeWageUsingSwitchCase commiting

--- src/ExtendsEmployee.java
+++ src/ExtendsEmployee.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -7,24 +8,33 @@ public class ExtendsEmployee extends Employee {
 	
 	//check employee attendance
 	protected void EmployeeAttendance() {
+		System.out.println("---------------------------------------");
 		if(randomNumber==1) {
 			logger.log(Level.INFO,"Employee present");	
 		}else {
 			logger.log(Level.INFO,"Employee absent");
 		}
+		System.out.println("---------------------------------------");
 	}
 	
 	//calculating daily wage of employee
 	protected double DailyEmployeeWage() {
-		double dailyWage=wagePerHour*fullDayHour;
+		System.out.println("---------------------------------------");
+		dailyWage=wagePerHour*fullDayHour;
 		logger.log(Level.INFO,"daily employee wage is :-"+dailyWage+" /- Rs");
+		System.out.println("---------------------------------------");
+
 		return dailyWage;
 	}
 	
 	//calculating daily wage of part time employee
 	protected void partTimeEmployeeWage() {
-		double partTimeEmployeeWage=wagePerHour*partTimeHour;
+		System.out.println("---------------------------------------");
+		partTimeEmployeeWage=wagePerHour*partTimeHour;
 		logger.log(Level.INFO,"Part time employee wage :-"+partTimeEmployeeWage+" /- Rs");
+		System.out.println("---------------------------------------");
 
 	}
+	
+
 }
